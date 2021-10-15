c15a93d6f70e22dfbc773d725291a00cef7227ac
Matthew Crawford <matthewcrawford@Matthews-MacBook-Air.local>
2021-10-14 17:37:52 -0700

    Created input form and started implementing add task functionality

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,49 @@ import React from 'react';
 class App extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            input: '',
+            todos: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(e) {
+        console.log('e.target.value: ', e.target.value)
+        this.setState({
+            input: e.target.value
+        })
+    }
+    handleSubmit() {
+        var sliced = this.state.todos.slice();
+        sliced.push(this.state.input)
+        console.log('sliced: ', sliced)
+        this.setState({
+            todos: sliced
+        })
     }
     render() {
         return(
             <div>
-                React working!
+                <div>
+                    To-Do List:
+                </div>
+                <form>
+                    <input onChange={this.handleChange}>
+                    </input>
+                    <button onClick={() => this.handleSubmit()}>
+                        Submit
+                    </button>
+                </form>
+                <div>
+                    {this.state.todos.map((todo) => {
+                        return (
+                            <div>
+                                {todo}
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
